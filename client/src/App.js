a676fd25bda59242fd7cbff0b133247c2d7b20fa
whatisthenote <jgj_junior@yahoo.com.br>
2020-03-05 18:36:07 -0300

    util funct

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,12 @@
 import React, { useEffect } from "react";
 import StocksXYValues from "./components/StocksXYValues";
 import Search from "./components/Search";
-import { connect } from "react-redux";
-import { setStockValues } from "./redux/actions";
+import fetchMyApi from "./utils/fetchMyApi";
 
-function App({ dispatch }) {
+function App() {
   useEffect(() => {
-    async function fetchMyApi() {
-      const values = await fetch("/values");
-      const valuesJson = await values.json();
-      const apiCall = await fetch(valuesJson.API_CALL);
-      const apiCallJson = await apiCall.json();
-      const stockValues = () => {
-        const entriesArray = Object.entries(apiCallJson["Time Series (Daily)"]);
-        const metaData = Object.values(apiCallJson["Meta Data"]);
-        const finalData = entriesArray.map(key => {
-          return {
-            name: key[0],
-            open: key[1]["1. open"],
-            close: key[1]["4. close"],
-            volume: key[1]["5. volume"],
-            symbol: metaData[1]
-          };
-        });
-        dispatch(setStockValues(finalData.reverse()));
-      };
-      stockValues();
-    }
     fetchMyApi();
-  }, [dispatch]);
+  }, []);
 
   return (
     <div>
@@ -38,4 +16,4 @@ function App({ dispatch }) {
   );
 }
 
-export default connect()(App);
+export default App;
---
+++ client/src/utils/fetchMyApi.js
@@ -0,0 +1,27 @@
+import { store } from "../redux/store";
+import { setStockValues } from "../redux/actions";
+
+async function fetchMyApi() {
+  const values = await fetch("/values");
+  const valuesJson = await values.json();
+  const apiCall = await fetch(valuesJson.API_CALL);
+  const apiCallJson = await apiCall.json();
+  const stockValues = () => {
+    const entriesArray = Object.entries(apiCallJson["Time Series (Daily)"]);
+    const metaData = Object.values(apiCallJson["Meta Data"]);
+    const finalData = entriesArray.map(key => {
+      return {
+        name: key[0],
+        open: key[1]["1. open"],
+        close: key[1]["4. close"],
+        volume: key[1]["5. volume"],
+        symbol: metaData[1]
+      };
+    });
+    store.dispatch(setStockValues(finalData.reverse()));
+  };
+  stockValues();
+}
+
+const a = fetchMyApi;
+export default a;
